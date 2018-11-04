a51bdb815f5f3507fe9a9a98db431c9196fc32d5
PESmereczniak <Philip3.8@hotmail.com>
2018-11-04 16:14:12 -0600

    Add project files.

---
+++ ChipStacker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChipStacker.Startup))]
+namespace ChipStacker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
